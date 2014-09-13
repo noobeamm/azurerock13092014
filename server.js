b3b6921b59104014afa650fffb91247bddc2f271
noobeamm <noobeamm@hotmail.com>
2014-09-13 10:54:19 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Azure Rock!\n This is make from Node.js \n 13 Sep 2014');
+}).listen(port);
